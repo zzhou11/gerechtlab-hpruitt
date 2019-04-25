6975cca00e9bada6cd3a9cec1e992c3d2dbaa6cd
zzhou11 <49929072+zzhou11@users.noreply.github.com>
2019-04-25 00:27:19 -0400

    Delete TCT_InitData.m

--- TCT_InitData.m
+++
@@ -1,9 +0,0 @@
-function [usableData] = TCT_InitData(DataTitle, usableCols)
-%   TCT_INITDATA Transforms a raw .xlsx into a processable matrix. usableCol
-%   indicates which columns are processed
-%   First two column MUST be Track number and slice number
-%   The same track numbers should be grouped together, NO ISOLATED TRACKS!
-rawData = xlsread(DataTitle);
-usableData = rawData(:,1:usableCols);
-
-end
