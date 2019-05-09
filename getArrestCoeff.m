4824f51b76a9774e5ed942c126cbacc9446c5305
hudsonliu <49970818+hudsonliu@users.noreply.github.com>
2019-05-09 10:52:25 -0400

    Add files via upload
    Removed an unnecessary variable

--- getArrestCoeff.m
+++ getArrestCoeff.m
@@ -1,4 +1,4 @@
-function [outputData] = getArrestCoeff(data,time_interval, threshold)
+function [outputData] = getArrestCoeff(data, threshold)
 %getArrestCoeff returns a matrix containing the arrest coefficient of each
 %track in an Nx2 maxtrix. Column 1 is the track number, column 2 is the
 %arrest coefficient.
@@ -7,7 +7,7 @@ outputData = [];
 maxTrackIndex = getMaxTrack(data);
 
 for i=1:maxTrackIndex;  %run through every track 
-    velocities = getVelocities(data,i,time_interval); %get a matrix with the velocities of the current track
+    velocities = getVelocities(data,i,1); %get a matrix with the velocities of the current track
     arrestCounter = 0; %this will count the number of time intervals where the cell is arrested
     velocitySize = size(velocities,1);
     if velocitySize == 0;
